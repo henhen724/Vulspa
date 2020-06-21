9d709cb9d508d47e6059d1164f10b3a8fe8e528b
henhen724 <hshunt@uchicago.edu>
2020-06-20 22:02:23 -0400

    quick implementation of !register

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -4,6 +4,6 @@ module.exports = {
     name: 'register',
     description: 'Setups up vulspa account for the discord user.',
     execute(msg: Message, args: string[], client: Client) {
-        msg.channel.send("This isn't implemented yet.");
+        msg.author.send("https://vulspa-io.herokuapp.com/accounts/signup")
     }
 }
